9a5d23bc9625ea75ca72c3ee54be218cc057dbcc
jayme-github <tuxnet@gmail.com>
2014-08-20 16:41:12 +0200

    Add setup file

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='newznabbalancer',
+    version="0.1",
+    description='balance your requests over different newznab providers',
+    author='Jayme',
+    author_email='tuxnet@gmail.com',
+    url='https://github.com/jayme-github/newznabbalancer',
+    packages=['newznabbalancer'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    scripts = ['newznabbalancer.py'],
+)
